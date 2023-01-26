8807d9c206e5023f4afa302166bb0718cf8bdd37
asherpage <110576723+asherpage@users.noreply.github.com>
2023-01-25 18:18:24 -0700

    Bernard ball

---
+++ soccer/soccer.js
@@ -0,0 +1,19 @@
+field.onclick = function(event){
+    let fieldCoords = this.getBoundingClientRect();
+    let ballCoordinates = {
+        top: event.clientY - fieldCoords.top - field.clientTop - ball.clientHeight / 2 - 10,
+        left: event.clientX - fieldCoords.left - field.clientLeft - ball.clientWidth / 2 - 7,
+    }
+    if(ballCoordinates.top < 0){ballCoordinates.top = 0;}
+    if(ballCoordinates.left < 0){ballCoordinates.left = 0;}
+  
+    if(ballCoordinates.top + ball.clientHeight > field.clientHeight){
+        ballCoordinates.top = field.clientHeight - ball.clientHeight
+    }
+    if(ballCoordinates.left + ball.clientWidth > field.clientWidth){
+        ballCoordinates.left = field.clientWidth - ball.clientWidth;
+    }
+
+    ball.style.left = ballCoordinates.left + "px";
+    ball.style.top = ballCoordinates.top + "px";
+}
